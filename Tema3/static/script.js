95f59c8fee86a5715760a16bba3e35aaafa030c5
GabrielNechifor <47883090+GabrielNechifor@users.noreply.github.com>
2020-04-02 13:28:34 +0300

    Add files via upload

---
+++ Tema3/static/script.js
@@ -0,0 +1,56 @@
+
+
+'use strict';
+
+
+window.addEventListener('load', function () {
+  document.getElementById('sign-out').onclick = function () {
+    firebase.auth().signOut();
+  };
+
+  // FirebaseUI config.
+  var uiConfig = {
+    signInSuccessUrl: '/',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+  };
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      document.getElementById('sign-out').hidden = false;
+      document.getElementById('login-info').hidden = false;
+      document.getElementById('upload').hidden = false;
+      console.log(`Signed in as ${user.displayName} (${user.email})`);
+      user.getIdToken().then(function (token) {
+
+        document.cookie = "token=" + token;
+        
+      });
+    } else {
+      // User is signed out.
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // Show the Firebase login button.
+      ui.start('#firebaseui-auth-container', uiConfig);
+      // Update the login state indicators.
+      document.getElementById('sign-out').hidden = true;
+      document.getElementById('login-info').hidden = true;
+      document.getElementById('upload').hidden = true;
+      // Clear the token cookie.
+      document.cookie = "token=";
+    }
+  }, function (error) {
+    console.log(error);
+    alert('Unable to log in: ' + error)
+  });
+});
+
