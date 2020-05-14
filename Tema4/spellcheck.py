bdd026710aaf3de2f50beb1cd74d08f05abfc0aa
GabrielNechifor <gabriel.nechifor98@gmail.com>
2020-05-14 17:23:59 +0300

    am adaugat tema 5

--- Tema4/spellcheck.py
+++ Tema4/spellcheck.py
@@ -1,12 +1,24 @@
 import urllib.request as request
 import urllib.parse as parse
 import json
-
+import requests
 
 
 def spellcheck_text(text, language):
-    api_key = "d4bd0d018cc241cebc1ec171af94e4e4"
-    endpoint = "https://lab9cc.cognitiveservices.azure.com/bing/v7.0/spellcheck"
+    api_key = "09129496065c485d81a8bc76bc242d2a"
+    endpoint = "https://tema4spellcheck.cognitiveservices.azure.com/bing/v7.0/spellcheck"
+    data = {'text': text}
+    params = {
+    'mkt': language,
+    'mode':'proof'
+    }
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Ocp-Apim-Subscription-Key': api_key,
+    }
+    response = requests.post(endpoint, headers=headers, params=params, data=data)
+    json_response = response.json()
+    '''
     data = {'text': text,
             'mkt': language,
             'mode': 'proof'}
@@ -19,8 +31,8 @@ def spellcheck_text(text, language):
     data = parse.urlencode(data).encode('utf-8')
     response = request.Request(endpoint, headers=headers, data = data)
     json_response = request.urlopen(response)
-
-    result = json.loads(json_response.read())
+    '''
+    result = json_response
     aditional_replacing_offset = 0
 
     for corrections in result['flaggedTokens']:
@@ -29,7 +41,7 @@ def spellcheck_text(text, language):
         correct_word = corrections['suggestions'][0]['suggestion'] #most likely word
         text = text[:offset] + correct_word + text[offset+len(bad_word):]
         aditional_replacing_offset += len(correct_word) - len(bad_word)
-
+    
     return text
 
 
